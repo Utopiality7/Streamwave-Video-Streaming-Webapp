9686ab27d940586f484b231eaf45e32b2d783434
utopiality7 <akirameruna7315@gmail.com>
2025-09-08 07:59:05 -0400

    built sidebar: hamburger click handling, category data fetching

--- src/context/ApiContext.jsx
+++ src/context/ApiContext.jsx
@@ -5,10 +5,12 @@ export const DataContext = createContext();
 
 export const AppContext = (props) => {
   const [loading, setLoading] = useState("loading");
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState("New");
 
   useEffect(() => {
-    fetchSelectedCategoryData();
-  }, []);
+    fetchSelectedCategoryData(selectedCategory);
+  }, [selectedCategory]);
 
   const fetchSelectedCategoryData = async (query) => {
     setLoading(true);
@@ -18,7 +20,16 @@ export const AppContext = (props) => {
   };
 
   return (
-    <DataContext.Provider value={{ loading, setLoading }}>
+    <DataContext.Provider
+      value={{
+        loading,
+        setLoading,
+        mobileMenu,
+        setMobileMenu,
+        selectedCategory,
+        setSelectedCategory,
+      }}
+    >
       {props.children}
     </DataContext.Provider>
   );
